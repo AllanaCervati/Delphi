7ea13c2be45bdecb70bc8484440c22bd7b9a2d95
unknown <allanacervati@gamil.com>
2022-08-04 21:15:29 -0300

    Projetos em Delphi

---
+++ prjCPF.dpr
@@ -0,0 +1,14 @@
+program prjCPF;
+
+uses
+  Vcl.Forms,
+  untCPF in 'untCPF.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
